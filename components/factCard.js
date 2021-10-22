58cad57865672308f7db9d7bbae8d9fefa1a3cc4
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-22 14:59:01 +0200

    ✨ fIntroduce facts API

--- components/factCard.js
+++ components/factCard.js
@@ -1,7 +1,7 @@
 import { createElement } from "../lib/elements.js";
 import styles from "./factCard.module.css";
 
-export function createFactCard({ fact }) {
+export function createFactCard(fact) {
   const factCard = createElement("article", {
     textContent: fact,
     className: styles.facts,
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import { createElement } from "./lib/elements.js";
 import "./style.css";
 import { createFactCard } from "./components/factCard";
-import fetchFacts from "/.lib/fetchFact.js";
+import fetchFact from "./lib/fetchFact";
 
 async function renderApp() {
   const appElement = document.querySelector("#app");
-
-  /*  const factCards = createFactCard(); */
 
   const headerElement = createElement("header", { className: "header" }, [
     createElement("h1", {
@@ -14,9 +12,11 @@ async function renderApp() {
     }),
   ]);
 
-  const facts = await fetchFacts();
+  const facts = await fetchFact();
 
-  const factCards = facts.map((fact) => createFactCard(fact));
+  const factCards = createFactCard(facts);
+
+  /* const factCards = facts.map((fact) => createFactCard(fact)); */
 
   const mainElement = createElement("main", { className: "main" }, [factCards]);
 
