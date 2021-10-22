393d23e86964a7d9df03eef46508e2de689a98ec
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-22 12:02:48 +0200

    :heavy_plus_sign: Fetch dog facts api and creae fact card component
    Fetch dog facts api and creae fact card component

--- components/factCard.js
+++ components/factCard.js
@@ -1,2 +1,11 @@
 import { createElement } from "../lib/elements.js";
 import styles from "./factCard.module.css";
+
+export function createFactCard({ fact }) {
+  const factCard = createElement("article", {
+    textContent: fact,
+    className: styles.facts,
+  });
+
+  return factCard;
+}
--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 import { createElement } from "./lib/elements.js";
 import "./style.css";
+import { createFactCard } from "./components/factCard";
+import fetchFacts from "/.lib/fetchFact.js";
 
 async function renderApp() {
   const appElement = document.querySelector("#app");
 
+  /*  const factCards = createFactCard(); */
+
   const headerElement = createElement("header", { className: "header" }, [
     createElement("h1", {
-      textContent: "RANDOM DOG FACTS",
+      textContent: "RANDOM DOG FACTS 🐶",
     }),
   ]);
 
-  const mainElement = createElement("main", { className: "main" });
+  const facts = await fetchFacts();
+
+  const factCards = facts.map((fact) => createFactCard(fact));
+
+  const mainElement = createElement("main", { className: "main" }, [factCards]);
 
   appElement.append(headerElement, mainElement);
 }
